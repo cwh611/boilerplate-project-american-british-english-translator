a9b75e26fc767bb06fbc7f00f261c1a1222adb42
cwh611 <chuck@MacBook-Pro-3.lan>
2024-12-27 17:21:06 -0800

    updates

--- public/index.js
+++ public/index.js
@@ -1,7 +1,7 @@
 document.getElementById("translate-btn").addEventListener("click", () => {
   const textInput = document.getElementById("text-input").value;
   const localeInput = document.getElementById("locale-input").value;
-  fetch("app.heroku/api/translate", {
+  fetch("https://chunk-english-translator-96133efd5b65.herokuapp.com/api/translate", {
     method: "POST",
     headers: {"Content-Type": "application/json"},
     body: JSON.stringify({
